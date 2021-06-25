10bece55a02e93ee5561efefa7769536336f7402
ffrozkhan <ffrozkhan@gmail.com>
2021-06-25 20:20:45 +0530

    Day-4.

--- src/Components/LearningCss.jsx
+++ src/Components/LearningCss.jsx
@@ -9,7 +9,7 @@ const LearningCss=()=>{
         };
         return(
         <div>
-            <h1 >  Learning Css in React JS </h1>
+            <h1 style={{color: "red"}}>  Learning Css in React JS </h1>
             <p style={stylePara}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam impedit laboriosam iste veniam libero esse harum illum voluptas facere tempora.</p>
             <p2 className="afroz">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, corporis.</p2>
         </div>
--- src/Components/Thirdfile.jsx
+++ src/Components/Thirdfile.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
 const Thirdfile=()=>{
+    const styleP={
+        backgroundColor: "Blue",
+        padding: "50px",
+        borderRadius: "20px",
+
+
+    }
     return(
         <div>
-            <h2> Resolving GitHub error </h2>
-            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, officiis? </p>
+            <h2 style={{color: "green"}}> Resolving GitHub error </h2>
+            <p style={styleP}> Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur, officiis? </p>
         </div>
     )
 }
