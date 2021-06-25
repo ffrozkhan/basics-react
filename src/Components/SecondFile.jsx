2c7119507b507c669b97f8feabd0dbfe91790e60
ffrozkhan <ffrozkhan@gmail.com>
2021-06-25 16:08:41 +0530

    Day-4

--- src/Components/SecondFile.jsx
+++ src/Components/SecondFile.jsx
@@ -4,7 +4,7 @@ import '../Components/LearningCss.css';
 const SecondFile = () => {
     return (
         <div>
-            <h1 className="afroz2">  Learning Css in React JS </h1>
+            <h1 className="afroz">  Learning Css in React JS </h1>
             <p className="afroz2"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam impedit laboriosam iste veniam libero esse harum illum voluptas facere tempora.</p>
             <p className="afroz"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, corporis.</p>
         
