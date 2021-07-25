4601bca1090a7a55b0b98106c73759bc6ad78f16
ffrozkhan <ffrozkhan@gmail.com>
2021-07-25 16:53:02 +0530

    Landing Practice

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -3,10 +3,13 @@ import Navbarm from '../Navbar/Navbarm';
 import Classcompo from '../Classcompo/Classcompo'
 import DemoClassComp from '../DemoClassComponent/DemoClassComp';
 import { UseStateHooks } from '../Header/Hooks/UseStateHooks';
+import Body from '../DemoClassComponent/Body';
 
 const Landing = () => {
     return (
         <div>
+            <Body/>
+
             <Navbarm/>
             <Classcompo/>
             <DemoClassComp/>
